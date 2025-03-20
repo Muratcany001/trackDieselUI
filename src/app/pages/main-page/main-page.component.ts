3aed6f7d12df6fb5e5b320c162e98a7d17a2ae22
mcyasar <muratcany0167@gmail.com>
2025-03-20 22:52:52 +0300

    #2# Main-page add-page ve update-page UI prototiplerinin oluşturulması

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
