fb380d83933e6d81ed2f0c4525923d4de48fec82
mcyasar <muratcany0167@gmail.com>
2025-04-03 23:20:50 +0300

    #3# Register page componentinin yazılması

--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from '../../restApiService/api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-page',
-  imports: [],
   templateUrl: './register-page.component.html',
-  styleUrl: './register-page.component.css'
+  styleUrls: ['./register-page.component.css']
 })
 export class RegisterPageComponent {
+  registerForm: FormGroup;
+  errorMessage: string = '';
 
+  constructor(
+    private fb: FormBuilder,
+    private apiService: ApiService,
+    private router: Router
+  ) {
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  register() {
+    if (this.registerForm.valid) {
+      this.apiService.login(this.registerForm.value).subscribe({
+        next: () => {
+          alert('Kayıt başarılı! Giriş yapabilirsiniz.');
+          this.router.navigate(['/login']);
+        },
+        error: (Error) => {
+          this.errorMessage = Error.message;
+        }
+      });
+    }
+  }
 }
